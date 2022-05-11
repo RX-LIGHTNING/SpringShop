6bb474fbfa5c815650712ab48d1012913ee242f3
Roman Kuznetsov <mmamano@inbox.ru>
2022-05-11 11:00:07 +0300

    init

---
+++ src/test/java/com/stationary/shop/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stationary.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
