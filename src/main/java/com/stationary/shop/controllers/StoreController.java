ee6eab6bb216fa451aefbca5df7638efbeb1acd3
Roman Kuznetsov <roman_kuzn@mail.ru>
2022-06-22 05:09:14 +0300

    added product search

--- src/main/java/com/stationary/shop/controllers/StoreController.java
+++ src/main/java/com/stationary/shop/controllers/StoreController.java
@@ -2,17 +2,26 @@ package com.stationary.shop.controllers;
 
 import com.stationary.shop.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Objects;
 
 @Controller
 public class StoreController {
     @Autowired
     ProductService productService;
     @GetMapping("/Store")
-    public String getShopPage(Model model){
-        model.addAttribute("products", productService.getProducts());
+    public String getShopPage(Model model, @Param("keyword") String keyword){
+        if(keyword!=null)
+        {
+            model.addAttribute("products", productService.getProdRepo().search(keyword));
+        }
+        else{
+            model.addAttribute("products", productService.getProducts());
+        }
         return "Store";
     }
 }
--- src/main/java/com/stationary/shop/repo/ProductRepo.java
+++ src/main/java/com/stationary/shop/repo/ProductRepo.java
@@ -2,6 +2,7 @@ package com.stationary.shop.repo;
 
 import com.stationary.shop.entities.Product;
 import com.stationary.shop.entities.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +10,6 @@ import java.util.List;
 
 @Repository
 public interface ProductRepo extends CrudRepository<Product,Long> {
+    @Query("SELECT p FROM Product p WHERE CONCAT(p.name, p.description, p.producer) LIKE %?1%")
+    List<Product> search(String keyword);
 }
